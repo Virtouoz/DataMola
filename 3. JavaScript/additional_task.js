414834a41abae8cf038ec2ef80171ee0fc786954
Aliaksandr <klut2018a@gmail.com>
2020-11-02 13:29:11 +0300

    Fix 1 task

--- 3. JavaScript/additional_task.js
+++ 3. JavaScript/additional_task.js
@@ -4,32 +4,35 @@
 * (содержащий как минимум одно число) с наибольшей суммой элементов и вернет эту сумму.
 */
 
-console.log('----------------------------');
-let array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-let start_index_subarray = 0, length = 1, length_subarray = 1, max_sum, new_sum;
-
-max_sum = new_sum = array[0];
-
-do {
-    for (let i = 0; i < array.length - length_subarray + 1; i++) {
-        for (let j = i; j < i + length_subarray; j++) {
-            new_sum += array[j];
-        }
-        if (max_sum <= new_sum) {                                    // Можно выводить самый короткий подмассив или саммый длинный: < иначе <=. Суть нулевой подмассив [1, 2, -3].
-            max_sum = new_sum;
-            start_index_subarray = i;
-            length = length_subarray;
-        }
-        new_sum = 0;
+function getMaxSubSum(array) {
+    if (array.length === 0) {
+        console.log('Error');
+        return 0;
     }
-} while (++length_subarray <= array.length);
+    let startIndexSubarray = 0, length = 1, lengthSubarray = 1, maxSum = newSum = 0;
 
-console.log('Max = ', max_sum, '!');
-console.log('Start index = ', start_index_subarray, ';');
+    do {
+        for (let i = 0; i < array.length - lengthSubarray + 1; i++) {
+            for (let j = i; j < i + lengthSubarray; j++) {
+                newSum += array[j];
+            }
+            if (maxSum < newSum) {
+                maxSum = newSum;
+                startIndexSubarray = i;
+                length = lengthSubarray;
+            }
+            newSum = 0;
+        }
+        lengthSubarray++;
+    } while (lengthSubarray <= array.length);
+    return maxSum;
+}
 
-console.log('Output subarray: ');
-for (let i = start_index_subarray; i < start_index_subarray + length; i++)
-    console.log(array[i]);
+let array = [100, -9, 2, -3, 5];
+getMaxSubSum(array);
+
+console.log('----------------------------');
+console.log('Max = ', getMaxSubSum(array), '!');
 console.log('----------------------------');
 
 /*  Задание 2
@@ -40,37 +43,45 @@ console.log('----------------------------');
 * продать акцию до того, как купит следующую (в любой момент времени у него не более одной акции).
 */
 
-console.log('----------------------------');
-let array1 = [7, 1, 5, 3, 6, 4];
-let array2 = [1, 2, 3, 4, 5];
-let array3 = [7, 6, 4, 3, 1];
+function getMaxSubArraySum(array) {
+    let sum = 0, index1 = 0, index2 = 0, flag = 0;
 
-let sum = 0, index1 = 0, index2 = 0, flag = 0;
-
-for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] < array[i + 1]) {
-        index1 = i;
-        for (i; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                index2 = i;
-                flag = 1;
-                break;
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] < array[i + 1]) {
+            index1 = i;
+            for (i; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    index2 = i;
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag) {
+                flag = 0;
+                sum += array[index2] - array[index1];
+                //console.log('array[index1]=', array[index1]);
+                //console.log('array[index2]=', array[index2]);
+            } else {
+                sum += array[array.length - 1] - array[index1];
+                //console.log('array[index1]=', array[index1]);
+                //console.log('array[array.length - 1]=', array[array.length - 1]);
             }
         }
-        if (flag) {
-            flag = 0;
-            sum += array[index2] - array[index1];
-            //console.log('array[index1]=', array[index1]);
-            //console.log('array[index2]=', array[index2]);
-        } else {
-            sum += array[array.length - 1] - array[index1];
-            //console.log('array[index1]=', array[index1]);
-            //console.log('array[array.length - 1]=', array[array.length - 1]);
-        }
     }
+    return sum;
 }
-console.log('Maximum profit = ',sum);
+
+let array1 = [-1, -2, -3, -4];
+let array2 = [-3, 2, 3, 2, 3];
+let array3 = [-4, -3, -2, -1];
+
+console.log('Maximum profit array1 = ', getMaxSubArraySum(array1));
 console.log('----------------------------');
+console.log('Maximum profit array2 = ', getMaxSubArraySum(array2));
+console.log('----------------------------');
+console.log('Maximum profit array3 = ', getMaxSubArraySum(array3));
+console.log('----------------------------');
+
 //let k = 0, k1 = 0; - нужны для посчета ниже
 /*for (let i = 0; i < array.length - 1; i++) {
     if (array[i] < array[i + 1])
